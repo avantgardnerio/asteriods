667a1ec261854b388aae72e631fbe5dda7d9e733
Brent Gardner <bgardner@squarelabs.net>
2015-11-03 10:48:00 -0700

    Keyboard input

--- main.js
+++ main.js
@@ -2,14 +2,32 @@ var cnvMain;
 var lastTick;
 var ctx;
 var pos = [0, 0];
-var velocity = [50, 50];
+var velocity = [200, 200];
+var keymap = {};
+
+var KEY_LEFT = 37;
+var KEY_UP = 38;
+var KEY_RIGHT = 39;
+var KEY_DOWN = 40;
 
 $(document).ready(function() {
+
+    // Set up canas
     cnvMain = $("#cnvMain")[0];
     cnvMain.width = window.innerWidth;
     cnvMain.height = window.innerHeight;
     ctx = cnvMain.getContext("2d");
 
+    // Add key listeners
+    $(window).keydown(function(ev) {
+        console.log(ev.keyCode);
+        keymap[ev.keyCode] = true;
+    });
+    $(window).keyup(function(ev) {
+        keymap[ev.keyCode] = false;
+    });
+
+    // Start animation loop
     requestAnimationFrame(render);
 });
 
@@ -20,11 +38,21 @@ function render(timestamp) {
         requestAnimationFrame(render);
         return;
     }
+    var delta = (timestamp - lastTick) / 1000;
 
     // Physics
-    var delta = (timestamp - lastTick) / 1000;
-    pos[0] += delta * velocity[0];
-    pos[1] += delta * velocity[1];
+    if(keymap[KEY_LEFT] === true) {
+        pos[0] -= delta * velocity[0];
+    }
+    if(keymap[KEY_RIGHT] === true) {
+        pos[0] += delta * velocity[0];
+    }
+    if(keymap[KEY_UP] === true) {
+        pos[1] -= delta * velocity[1];
+    }
+    if(keymap[KEY_DOWN] === true) {
+        pos[1] += delta * velocity[1];
+    }
 
     // Rendering
     ctx.clearRect(0, 0, cnvMain.width, cnvMain.height);
